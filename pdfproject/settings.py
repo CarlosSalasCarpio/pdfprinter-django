1ed730d77b29b7c62cc775cb89c4b9acf01aef2d
Carlos Salas <carlos.salascarpio@gmail.com>
2022-01-26 21:24:05 -0500

    tect editor complete

--- pdfproject/settings.py
+++ pdfproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pdf',
+    'pdfcreator',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
